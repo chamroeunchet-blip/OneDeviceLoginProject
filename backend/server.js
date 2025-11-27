19868708b5d84931817e1b4826f9bf718241b7ed
chamroeunchet-blip <chamroeunchet@gmail.com>
2025-11-28 00:35:08 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ function saveDB(data) {
 
 // === Users Config ===
 const USERS = [
-  { username: "Mr1", password: "7777" },
+  { username: "Yuos_chamroeun", password: "chamroeun@2025" },
   { username: "Mr2", password: "8888" },
   { username: "Mr3", password: "9999" },
   { username: "Mr4", password: "1111" }

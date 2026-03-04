611c9f589288c8db89248ff98285c49ba013d1b5
chamroeunchet-blip <chamroeunchet@gmail.com>
2026-03-04 10:13:10 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -167,6 +167,7 @@ const USERS = [
 { username: "HeangphonMD10", password: "Phon@168", startDate: "2026-4-03", durationDays: 366},
 { username: "2HeangphonMD10", password: "Phon@168", startDate: "2026-4-03", durationDays: 366},
   { username: "3HeangphonMD10", password: "Phon@168", startDate: "2026-4-03", durationDays: 366},
+  { username: "Vannak", password: "Nak168168", startDate: "2026-4-03", durationDays: 366},
   
   { username: "Lormengkorng33", password: "1", startDate: "2026-4-03", durationDays: 366},
 
